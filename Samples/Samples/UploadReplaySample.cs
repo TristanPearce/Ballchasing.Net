9b6becf95627b7e2d768bd57773df9f67cf8897e
Tristan Pearce <tristanpearce24@gmail.com>
2021-08-07 22:55:41 +0100

    Add UploadReplay sample. Some required bits added too.

---
+++ Samples/Samples/UploadReplaySample.cs
@@ -0,0 +1,46 @@
+﻿using Ballchasing.Net;
+
+using Samples.Net;
+using Samples.Utils;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.Samples
+{
+
+    [Sample]
+    public class UploadReplaySample
+    {
+
+        [Run]
+        public async Task UploadReplay(IBallchasingAPI api)
+        {
+            // This loads an example replay from embedded data
+            EmbeddedResourceLoader loader = new EmbeddedResourceLoader(typeof(UploadReplaySample).Assembly, "Samples.Data.");
+
+            string replayFileName = "my_2v2.replay";
+
+            // This gets the stream to the replay file. You would probably use File.Open to read a real replay from disk.
+            using Stream replay = loader.GetResource(replayFileName);
+
+            // this uploads the replay
+            var result = await api.UploadReplayAsync(replay, replayFileName, Privacy.Private);
+
+            if (result.Success)
+            {
+                Console.WriteLine($"ID: {result.Upload.Id}");
+                Console.WriteLine($"Location: {result.Upload.Location}");
+            }
+            else
+            {
+                Console.WriteLine("Replay failed to upload");
+                Console.WriteLine(result.Error);
+            }
+        }
+    }
+}
---
+++ Samples/Utils/EmbeddedResourceLoader.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Samples.Utils
+{
+    public class EmbeddedResourceLoader
+    {
+        private Assembly _assembly;
+        private string _prefix;
+
+        public Assembly Assembly => _assembly;
+        public string Prefix => _prefix;
+
+        public EmbeddedResourceLoader(Assembly assembly, string prefix)
+        {
+            _assembly = assembly;
+            _prefix = prefix;
+        }
+
+        public string[] GetResourceNames()
+        {
+            return _assembly.GetManifestResourceNames();
+        }
+
+        public string[] GetResourceNames(string prefix = "", string contains = "", string suffix = "")
+        {
+            string[] all = _assembly.GetManifestResourceNames();
+            return all.Where(x => x.StartsWith(prefix)).Where(x => x.Contains(contains)).Where(x => x.EndsWith(suffix)).ToArray();
+        }
+
+        public Stream GetResource(string name)
+        {
+            string path = _prefix + name;
+            Stream s = _assembly.GetManifestResourceStream(path);
+
+            if (s == null)
+                throw new EmbeddedResourceNotFound(path);
+
+            return s;
+        }
+
+        public string GetResourceString(string name)
+        {
+            Stream stream = _assembly.GetManifestResourceStream(_prefix + name);
+            StreamReader streamReader = new StreamReader(stream);
+
+            string result = streamReader.ReadToEnd();
+
+            stream.Dispose();
+            streamReader.Dispose();
+
+            return result;
+        }
+    }
+
+    public class EmbeddedResourceNotFound : Exception
+    {
+
+        public EmbeddedResourceNotFound() : base()
+        {
+
+        }
+
+        public EmbeddedResourceNotFound(string message) : base(message)
+        {
+
+        }
+
+        public EmbeddedResourceNotFound(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+
+        protected EmbeddedResourceNotFound(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+    }
+}
